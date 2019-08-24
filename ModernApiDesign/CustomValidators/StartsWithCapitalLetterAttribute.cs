47335131911a77508b1baffe6d90252ac98cad9c
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-08-24 18:47:05 +0200

    Custom Validation Implementation.

---
+++ ModernApiDesign/CustomValidators/StartsWithCapitalLetterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernApiDesign.CustomValidators
+{
+    public class StartsWithCapitalLetterAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value?.ToString()[0] != value?.ToString().ToUpper()[0])
+            {
+                return new ValidationResult("Value must start with capital letter");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ModernApiDesign/People/PersonDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernApiDesign.People
+{
+    public class PersonDto: IValidatableObject
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private bool StartsWithCaps(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value) && value[0].Equals(value.ToUpper()[0]);
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            const string message = "Value must start with capital letter";
+            var results = new List<ValidationResult>();
+            if (!StartsWithCaps(FirstName))
+            {
+                yield return new ValidationResult(message, new[] { nameof(FirstName) });
+            }
+            if (!StartsWithCaps(LastName))
+            {
+                yield return new ValidationResult(message, new[] { nameof(LastName) });
+            }
+        }
+    }
+}
